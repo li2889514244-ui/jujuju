ca313c065a7ae044620d2cba52628c416c4aafaa
li2889514244-ui <li2889514244@gmail.com>
2026-05-13 14:21:54 +0800

    "fix-remove-uploader-contentmodule"

--- backend/src/modules/content/content.module.ts
+++ backend/src/modules/content/content.module.ts
@@ -1,11 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { UploaderModule } from '../uploader/uploader.module';
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => UploaderModule)],
+  imports: [PrismaModule],
   controllers: [ContentController],
   providers: [ContentService],
   exports: [ContentService],
