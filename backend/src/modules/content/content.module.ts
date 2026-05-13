9cd02195a35d45a1ce6c886e458128cff71356ee
li2889514244-ui <li2889514244@gmail.com>
2026-05-13 14:28:41 +0800

    "fix-restore-uploader-forwardref"

--- backend/src/modules/content/content.module.ts
+++ backend/src/modules/content/content.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { UploaderModule } from '../uploader/uploader.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => UploaderModule)],
   controllers: [ContentController],
   providers: [ContentService],
   exports: [ContentService],
